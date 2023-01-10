72246829060df03f064a9b8172765c952a12add7
HectorV <hectorfabiovasquez@gmail.com>
2023-01-10 07:03:16 -0500

    Added State Hook to consume a rest api

--- src/App.js
+++ src/App.js
@@ -1,23 +1,33 @@
-import logo from './logo.svg';
-import './App.css';
+import React, { useState, useEffect } from "react";
+import "./App.css";
 
 function App() {
+  const url = "https://jsonplaceholder.typicode.com/todos";
+  const [todos, setTodos] = useState();
+  const fetchApi = async () => {
+    const response = await fetch(url);
+    // console.log(response.status)
+    const responseJson = await response.json();
+    setTodos(responseJson);
+    // console.log(responseJson)
+  };
+  useEffect(() => {
+    fetchApi();
+  }, []);
   return (
     <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+      Hola Mundo
+      <ul>
+        {!todos
+          ? "Cargando"
+          : todos.map((todo, index) => {
+              return (
+                <li key={index}>
+                  {todo.title} {todo.completed ? "✅" : "❌"}
+                </li>
+              );
+            })}
+      </ul>
     </div>
   );
 }
